ca4b3a6dbb93dea15f26f12e1c576d789e2d65e9
robin l'hyver <robinlhyver@gmail.com>
2023-11-15 15:58:01 +0100

    Debut du projet
    Construction du corps

---
+++ Projet-A2-S1/Projet-A2-S1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Projet_A2_S1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
