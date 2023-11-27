6fa945cda23359a6ec4acb8e35f635e396d402e6
robin l'hyver <robinlhyver@gmail.com>
2023-11-27 17:47:46 +0100

    Refactor menu logic and add parameter options

--- Projet-A2-S1/Projet-A2-S1/Program.cs
+++ Projet-A2-S1/Projet-A2-S1/Program.cs
@@ -8,37 +8,73 @@ namespace Projet_A2_S1
     {
         static void Main(string[] args)
         {
-
             Core.ClearWindow();
-            Core.SetTitle("COUCOU");
+            Core.SetTitle("Jeu des mots");
             Core.WriteTitle();
             Core.WriteBanner();
-            (int a , int b)= Core.ScrollingMenuSelector(" Menu Principal :", default, default, "Jouer", "Paramètre", "Quitter le jeu");
-            switch(b){
-                case 0:
-                    int number = TimedNumberInput(5);  // 5 seconds time limit
-                    if (number == -1)
-                    {
-                        Console.WriteLine("You didn't enter a number in time.");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"You entered: {number}");
-                    }
-                break;
-                case 1:
-                    Console.WriteLine("liste des paramètres ");
-                    (a,b)=Core.ScrollingMenuSelector("Liste des paramètres :",default , default, "Changer couleur","Changer langue ","Revenir au menu principal");
-                break;
-                case 2 : 
-                    
-                    Core.ExitProgram();
-                break;                   
+            bool EndMenu = true;
+            while(EndMenu){
+                var index= Core.ScrollingMenuSelector(" Menu Principal :", default, default, "Jouer", "Paramètre", "Quitter le jeu");
+                switch(index.Item1){
+                    case 0:
 
+                            switch(index.Item2){
+                            case 0:
+                                int number = TimedNumberInput(5);  // 5 seconds time limit
+                                if (number == -1)
+                                {
+                                    Console.WriteLine("You didn't enter a number in time.");
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"You entered: {number}");
+                                }
+                                EndMenu = false;
+                            break;
+                            
+                            case 1:
+                                var index2 =Core.ScrollingMenuSelector("Liste des paramètres :",default , default, "Changer couleur","Changer langue ","Revenir au menu principal");
+                                switch(index2.Item2){
+                                    case 0 : 
+                                        var color = Core.ScrollingMenuSelector("Choisir une couleur :",default,default,"Rouge","Bleu","Vert","Jaune","Blanc","Noir");
+                                        ConsoleColor[] colors = { ConsoleColor.Red, ConsoleColor.Blue, ConsoleColor.Green, ConsoleColor.Yellow, ConsoleColor.White, ConsoleColor.Black, ConsoleColor.Gray };
+                                        ConsoleColor couleur = colors[color.Item2];
+                                        Core.ChangeForeground(couleur);
+                                        Core.WritePositionedString("Couleur choisie : ",Placement.Center,default,10,default);
+                                    break;
+                                }
+                            break;
+
+                            case 2 : 
+                                EndMenu = false;
+                                Core.ExitProgram();
+                            break;                   
+                        }
+                    break;
+                    case -1: 
+                            Console.WriteLine("Vous avez pressé la touche echap, vous allez sortir du jeu, pressez entrée pour confirmer");
+                            Console.ReadKey();
+                            Core.ExitProgram();
+                    break;
+                    case -2: 
+                            Console.WriteLine("Vous avez pressé la touche backspace, vous allez sortir du jeu, pressez entrée pour confirmer");
+                            Console.ReadKey();
+                            Core.ExitProgram();
+                    break;
+                }
+                
+                
             }
+            
         }
 
 
+
+        /// <summary>
+        /// Fonction permettant de laisser un temps donné à l'utilisateur pour faire une action
+        /// </summary>
+        /// <param name="timeLimit"> variable qui permet de stocker la limite de temps donnée</param>
+        /// <returns></returns>
 
         public static int TimedNumberInput(int timeLimit)
         {
@@ -70,8 +106,6 @@ namespace Projet_A2_S1
                 {
                     return -1;
                 }
-
-
                 if (int.TryParse(input, out int number))
                 {
                     timer.Dispose();
