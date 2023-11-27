ad4323d6611476bec9d081c474d02ab2f85024ba
robin l'hyver <robinlhyver@gmail.com>
2023-11-27 21:01:36 +0100

    Add beginning method to Jeu class

--- Projet-A2-S1/Projet-A2-S1/Jeu.cs
+++ Projet-A2-S1/Projet-A2-S1/Jeu.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ConsoleAppVisuals;
 namespace Projet_A2_S1
 {
     internal class Jeu
     {
+
+        static public void beginning(){
+            Core.ClearWindow();
+            var index = Core.ScrollingNumberSelector("Choisir un nombre de joueur :",1, 4,1,1);
+            for(int i=0; i<index.Item2;i++){
+                Console.WriteLine("Entrez le nom du joueur :");
+                string nom = Console.ReadLine();
+                ConsoleColor color;
+                if(i==0){
+                    Console.WriteLine("coucou");
+                     color = ConsoleColor.Blue;
+                }
+                else if(i==1){
+                     color = ConsoleColor.Green;
+                }
+                else{
+                     color = ConsoleColor.Red;
+                }
+                Joueur coucou = new Joueur(nom,120,color);
+                Core.ChangeForeground(color);
+                string s = coucou.toString();
+                Core.WritePositionedString(s,Placement.Right,default,2*i+9,default);
+                Console.WriteLine();
+                Console.WriteLine();
+                
+            }
+        }
     }
 }
--- Projet-A2-S1/Projet-A2-S1/Joueur.cs
+++ Projet-A2-S1/Projet-A2-S1/Joueur.cs
@@ -2,13 +2,37 @@
 
 internal class Joueur
 {
+    string nom;
+    int score;
+    List<string> mots;
+    
+    int chrono;
+
+    ConsoleColor color; // couleur du joueur
+    public Joueur(string nom,int chrono, ConsoleColor color)
+    {
+        this.nom = nom;
+        this.score = 0;
+        this.mots = new List<string>();
+        this.chrono = chrono;
+        this.color = color;
+    }
+    string Nom { get => nom; }
+    int Score { get => score; }
+    List<string> Mots { get => mots; }
+    int Chrono { get => chrono; }
+
+    public string toString()
+    {
+        return $"Nom : {this.nom} \nScore : {this.score}     Timer : {chrono}\nMots : {this.mots}";
+    }
+}
+/*
 public void Add_Mot (string mot) 
 {
+    
 }
-public string toString()
-{
 
-}
 public void Add_Score(int val) 
 {
 
@@ -19,3 +43,4 @@ public bool Contient (string mot)
 }
 
 }
+*/
